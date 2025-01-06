90f16851b39ee10109591c66f9de222bc212ffd5
ZOPHONIASSON HARALD <harald@zophoniasson.fr>
2025-01-06 12:20:29 +0100

    Added the second PAMI to the config files

--- 2025/pami/info/pami_src/config_control.h
+++ 2025/pami/info/pami_src/config_control.h
@@ -23,9 +23,29 @@
 
 
 /******************************************************************************
-   This is the mechanical configuration of a typical PAMI robot
+   This is the mechanical configuration of the PAMI 1
  ******************************************************************************/
-#ifdef PAMI
+#ifdef PAMI_1
+
+#define KP_DISTANCE         0.6
+#define KI_DISTANCE         0.0
+#define KD_DISTANCE         0.0 // 0.0005
+
+#define KP_ORIENTATION      1.0
+#define KI_ORIENTATION      0.0
+#define KD_ORIENTATION      0.0 // 0.0005
+
+#define VITESSE_SLOW        0.4   /* [m.s-1] */
+#define ACCELERATION_SLOW   0.4   /* [m.s-2] */
+#define VITESSE_MAX         0.3   /* [m.s-1] */
+#define ACCELERATION_MAX    0.4   /* [m.s-2] */
+
+#endif
+
+/******************************************************************************
+   This is the mechanical configuration of the PAMI 2
+ ******************************************************************************/
+#ifdef PAMI_2
 
 #define KP_DISTANCE         0.6
 #define KI_DISTANCE         0.0
