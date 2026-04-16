446b0a96eac77bd8628149a20112ff616f36337d
ZOPHONIASSON HARALD <harald@zophoniasson.fr>
2026-04-16 14:16:02 +0200

    Encoder switched to full quadrature, so measuring is twice as precise.

--- 2025/robot/info/robot_core_src/config_meca.h
+++ 2025/robot/info/robot_core_src/config_meca.h
@@ -14,7 +14,7 @@
  * tops per motor revolution. With the reduction ratio this makes 28x30 = 840
  * tops per wheel rotation.
  */
-#define N_TOP_PER_WHEEL_TURN  2*500.0    /* Encoder top number per wheel turn [-] */
+#define N_TOP_PER_WHEEL_TURN  4.0*500.0    /* Encoder top number per wheel turn [-] */
 #define FACTOR_DISTANCE       0.9308820107051 /* Factor to adjust the top count */
 #define FACTOR_WHEEL_LEFT     1.0
 #define FACTOR_WHEEL_RIGHT    1.0
@@ -25,7 +25,7 @@
  */
 #define WHEEL_DIAMETER_M      0.040     /* Wheel diameter [m] */
 //#define MILLIMETER_TO_TOP          N_TOP_PER_WHEEL_TURN * FACTOR_DISTANCE / (WHEEL_DIAMETER_M * PI);
-#define MILLIMETER_TO_TOP          (7.957747154 * FACTOR_DISTANCE)
+#define MILLIMETER_TO_TOP          (2.0 * 7.957747154 * FACTOR_DISTANCE)
 
 /*
  * To determine the distance needed to rotate of a certain angle, one has to 
@@ -38,7 +38,7 @@
  */
 #define WHEELBASE_M           0.250 * 0.99802005834  /* Wheelbase of the robot [m] */
 //#define RAD_TO_TOP            N_TOP_PER_WHEEL_TURN / (WHEEL_DIAMETER_M / WHEELBASE_M ) / PI
-#define RAD_TO_TOP            (1989.4367886486 * FACTOR_DISTANCE)
+#define RAD_TO_TOP            (2.0 * 1989.4367886486 * FACTOR_DISTANCE)
 
 #define BACK_LENGTH           0.0 /* Length between wheel center and the back of the ROBOT_CORE */
 
