20667276cbca049a16469bf3e1ffb562d2cc5dc5
ZOPHONIASSON HARALD <harald@zophoniasson.fr>
2026-05-11 09:33:36 +0200

    Commented the actuator.cpp file to enable use of Lidar.

--- 2025/robot/info/robot_core_src/actuator.h
+++ 2025/robot/info/robot_core_src/actuator.h
@@ -1,13 +1,13 @@
-#ifndef actuator_h_
-#define actuator_h_
+// #ifndef actuator_h_
+// #define actuator_h_
 
-/******************************************************************************
- * Function Declarations
- ******************************************************************************/
-void ActuatorInit();
-void ActuatorUpdate(bool timeMeasure_b);
+// /******************************************************************************
+//  * Function Declarations
+//  ******************************************************************************/
+// void ActuatorInit();
+// void ActuatorUpdate(bool timeMeasure_b);
 
-void ActuatorStepperMove(uint8_t id_u8, int16_t stepNb_i16);
-void ActuatorStepperStop(uint8_t id_u8);
+// void ActuatorStepperMove(uint8_t id_u8, int16_t stepNb_i16);
+// void ActuatorStepperStop(uint8_t id_u8);
 
-#endif
+// #endif
